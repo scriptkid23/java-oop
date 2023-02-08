cb090b742effae5923fdface894d314b558cde31
hoandoice <hoan.do@icetea.io>
2023-02-08 23:21:05 +0700

    Update LocalClassExample.java

--- day04/LocalClassExample.java
+++ day04/LocalClassExample.java
@@ -16,4 +16,14 @@ public class LocalClassExample {
         var localClassExample = new LocalClassExample();
         localClassExample.calculate();
     }
+    /*
+     * 
+     * Why Can Local Classes Only Access final or Effectively Final Variables?
+     * 
+     *   The compiler generates a separate .class file for each inner class. 
+     *   A separate class has no way to refer to a local variable. However, if the local variable
+     *   is final or effectively final, Java can handle it by passing a copy of the value or reference
+     *   variable to the constructor of the local class. If it weren’t final or effectively final, these
+     *   tricks wouldn’t work because the value could change after the copy was made.
+     */
 }
